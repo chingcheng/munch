e6d1c79d58e0f74ea2d7dca8e65da2f42712fba8
Matthew <matthewlmai16@gmail.com>
2023-03-03 16:13:05 -0800

    removed from .get in main.py

--- munch/main.py
+++ munch/main.py
@@ -13,8 +13,7 @@ app.include_router(authenticator.router)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        os.environ.get("CORS_HOST", "http://localhost:3000",
-                       "https://munch.nov-pt-10.mod3projects.com")
+        os.environ.get("CORS_HOST", "http://localhost:3000")
     ],
     allow_credentials=True,
     allow_methods=["*"],
