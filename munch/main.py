1173704bf1a26f3f36f3f92056088b80495d3f37
Matthew <matthewlmai16@gmail.com>
2023-03-03 17:46:04 -0800

    deploy test2

--- munch/main.py
+++ munch/main.py
@@ -10,14 +10,14 @@ app.include_router(accounts.router)
 app.include_router(comments.router)
 app.include_router(authenticator.router)
 
-origins = [
-    "http://localhost:3000",
-    os.environ.get("CORS_HOST", "https://munchbunch.gitlab.io/")
-]
+# origins = [
+#     # "http://localhost:3000",
+#     # os.environ.get("CORS_HOST", "https://munchbunch.gitlab.io/")
+# ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
