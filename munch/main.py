6f27e5fcb96364dffc018e05c48a2a2362e64a7e
Matthew <matthewlmai16@gmail.com>
2023-03-06 22:27:07 -0800

    included localhost and api host url as a part of origins

--- munch/main.py
+++ munch/main.py
@@ -11,14 +11,17 @@ app.include_router(comments.router)
 app.include_router(authenticator.router)
 
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=[
-        os.environ.get(
+origins = [
+    "http://localhost:3000",
+    os.environ.get(
             "CORS_HOST",
             "https://munch.nov-pt-10.mod3projects.com"
         )
-    ],
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
